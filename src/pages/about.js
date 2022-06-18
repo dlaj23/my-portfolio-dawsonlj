f011da8887ab58d8be6f4a989d76eeb0be949fe6
Dawson Johnson <dlaj23@outlook.com>
2022-06-17 21:19:59 -0400

    Module 2

---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+// Step 1: Import React
+import * as React from 'react'
+import  { Link } from 'gatsby'
+
+// Step 2: Define component
+const AboutPage = () => {
+    return (
+        <main>
+            <title>Dawson L.A. Johnson</title>
+            <h1>About</h1>
+            <p>blah blah blahhhhh.</p>
+            <Link to="/">Home</Link>
+        </main>
+    )
+}
+
+// Step 3: Export component
+
+export default AboutPage
